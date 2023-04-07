c2dd76d7de616960564c7e8d6152a7df140e5f2e
Nicoargento11 <valdes.nicoedu@gmail.com>
2023-04-07 16:20:27 -0300

    feat(RegisterModal): onSubmit estructura

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -9,7 +9,36 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const RegisterModal = () => {
-  return <div>RegisterModal</div>;
+  const RegisterModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios
+      .post("api/register", data)
+      .then(() => {
+        RegisterModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  return <div></div>;
 };
 
 export default RegisterModal;
