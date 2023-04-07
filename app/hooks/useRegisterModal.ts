f352251ad6996a70188a21b80929c8bba811a6fd
Nicoargento11 <valdes.nicoedu@gmail.com>
2023-04-07 15:34:59 -0300

    feat(useRegisterModal): creacion del hook para el registro

--- app/hooks/useRegisterModal.ts
+++ app/hooks/useRegisterModal.ts
@@ -12,4 +12,4 @@ const useRegisterModal = create<RegisterModalStore>((set) => ({
     onClose: () => set({isOpen : false}),
 }));
 
-export default useRegisterModal
+export default useRegisterModal;
