7163479f7acb9e9b5d044c9c60f9ca5c336e8fda
Nicoargento11 <valdes.nicoedu@gmail.com>
2023-04-07 13:53:14 -0300

    feat: auth UI start

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/navbar/NavBar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 export const metadata = {
   title: "AirBnb-clone",
@@ -18,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <NavBar />
+        <ClientOnly>
+          <Modal isOpen tittle={`What's up`} />
+          <NavBar />
+        </ClientOnly>
         {children}
       </body>
     </html>
