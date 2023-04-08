1f3da8a63d3555cfb71a279b3448f04aa4a3cb07
Nicoargento11 <valdes.nicoedu@gmail.com>
2023-04-07 22:04:04 -0300

    feat(Input): interface

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,5 +1,27 @@
 "use client";
-const Input = () => {
+
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+}
+const Input: React.FC<InputProps> = ({
+  errors,
+  id,
+  label,
+  register,
+  disabled,
+  formatPrice,
+  required,
+  type,
+}) => {
   return <div>Input</div>;
 };
 
