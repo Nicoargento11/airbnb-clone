519deacc186e3506a27ce3c4ace65538a5f38a3a
Nicoargento11 <valdes.nicoedu@gmail.com>
2023-04-08 15:16:04 -0300

    feat(Auth UI): Auth UI completado

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import NavBar from "./components/navbar/NavBar";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
   title: "AirBnb-clone",
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <NavBar />
         </ClientOnly>
